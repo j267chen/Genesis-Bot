8d10cbb1df32cb7028306b4060bb212c7df7705c
jasonchen <j267chen@uwaterloo.ca>
2024-07-03 19:56:50 -0400

    version 1.2 of maze solving algorithm
    debugged using colour sheets of paper.

--- Software/ROBOTC/main.c
+++ Software/ROBOTC/main.c
@@ -1,3 +1,120 @@
+void configureAllSensors();
+
+void goRobot(int motorPower);
+
+//void mazeSolve();
+
+void rotateRobot(float angle, int motorPower);
+
+/*
+void turnLeft()
+
+bool checkFinish()
+{
+    if (SensorValue[S3] == (int)colorGreen)
+    {
+        unsolved = false;
+        timetofinish = time1[T1];
+        // is unsolved in scope?
+    }
+}
+*/
+
+
+
+task main() {
+    configureAllSensors();
+
+    displayString(2, "Maze Runner");
+    displayString(3, "Made by Jason Chen, Zachary Barandino, Eric Mak, and Eric Shaw");
+    displayString(5, "Press Enter to start");
+    while (!(getButtonPress(buttonEnter)))
+    {}
+    while (getButtonPress(buttonEnter))
+    {}
+
+    const int MOTPOWER = 10;
+    const int MOTSPINPOWER = 10;
+
+    float timetofinish = 0;
+    time1[T1] = 0;
+
+    //version 1.1 of maze solving: (turn left algorithm which includes intersection lights)
+    bool unsolved = true, blockobtained = true;
+    while (unsolved)
+    {
+        goRobot(MOTPOWER);
+        while (SensorValue[S3] == (int)colorBlack)
+        {}
+        goRobot(0);
+
+        //check for finish
+        if (SensorValue[S3] == (int)colorGreen)
+        {
+            if(blockobtained)
+            {
+                unsolved = false;
+                timetofinish = time1[T1];
+            }
+        }
+        //turn left algorithm at turns or ends of paths
+        else if (SensorValue[S3] == (int)colorRed)
+        {
+            //TO DO: have it move a certain mimimum distance
+            do
+            {
+                rotateRobot(90, MOTSPINPOWER);
+                nMotorEncoder[motorA] = 0;
+                goRobot(10); // go until new colour - check if path is available
+                while(SensorValue[S3] == (int)colorRed)
+                {}
+                goRobot(0);
+
+                if (SensorValue[S3] == (int)colorGreen)
+                {
+                    unsolved = false;
+                    timetofinish = time1[T1];
+                }
+                else if (SensorValue[S3] != (int)colorBlack) //go back
+                {
+                    goRobot(-MOTPOWER);
+                    while(nMotorEncoder[motorA] > 0)
+                    {}
+                    goRobot(0);
+                }
+            } while (SensorValue[S3] != (int)colorBlack || SensorValue[S3] != (int)colorGreen); //does the turn left algorithm until it detects black or green
+        }
+        else //accounts for turning error in both directions
+      	{
+            for(int i = 1; i < 6; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    rotateRobot(5 * i, MOTSPINPOWER);
+                    if (SensorValue[S3] == (int)colorBlack)
+                        i += 10;
+                }
+                else 
+                {
+                    rotateRobot(-(5 * i), MOTSPINPOWER); 
+                    if (SensorValue[S3] == (int)colorBlack)
+                        i += 10;
+                }
+            }
+    	}
+  	}
+    /*
+    need to introduce a secondary objective: pick up block and sort it
+
+    could introduce a way to recognize turns and adjust accordingly
+    could introduce A* algorithm to solve maze
+    */
+    displayString(5, "Maze Solved!");
+    displayString(6, "Time: %f s", timetofinish);
+    wait1Msec(10000);
+}
+
+
 //all code
 //#include "EV3_API.h"
 
@@ -38,95 +155,4 @@ void rotateRobot(float angle, int motorPower){
     while(abs(getGyroDegrees(S4)) < abs(angle))
     {}
     goRobot(0);
-}
-
-/*
-void turnLeft()
-
-bool checkFinish()
-{
-    if (SensorValue[S3] == (int)colorGreen)
-    {
-        unsolved = false;
-        timetofinish = time1[T1];
-        // is unsolved in scope?
-    }
-}
-*/
-
-task main() {
-    configureAllSensors();
-
-    displayString(2, "Maze Runner");
-    displayString(3, "Made by Jason Chen, Zachary Barandino, Eric Mak, and Eric Shaw");
-    displayString(5, "Press Enter to start");
-    while (!(getButtonPress(buttonEnter)))
-    {}
-    while (getButtonPress(buttonEnter))
-    {}
-
-    float timetofinish = 0;
-    time1[T1] = 0;
-
-    //version 1.1 of maze solving: (turn left algorithm which includes intersection lights)
-    bool unsolved = true;
-    while (unsolved)
-    {
-        goRobot(25);
-        while (SensorValue[S3] == (int)colorBlack)
-        {}
-        goRobot(0);
-
-        //check for finish
-        if (SensorValue[S3] == (int)colorGreen)
-        {
-            unsolved = false;
-            timetofinish = time1[T1];
-        }
-        //turn left algorithm at turns or ends of paths
-        else if (SensorValue[S3] == (int)colorRed)
-        {
-            do
-            {
-                rotateRobot(90, 25);
-                nMotorEncoder[motorA] = 0;
-                goRobot(10); // go until new colour - check if path is available
-                while(SensorValue[S3] == (int)colorRed)
-                {}
-                goRobot(0);
-
-                if (SensorValue[S3] == (int)colorGreen)
-                {
-                    unsolved = false;
-                    timetofinish = time1[T1];
-                }
-                else if (SensorValue[S3] != (int)colorBlack) //go back
-                {
-                    goRobot(-10);
-                    while(nMotorEncoder[motorA] > 0)
-                    {}
-                    goRobot(0);
-                }
-            } while (SensorValue[S3] != (int)colorBlack || SensorValue[S3] != (int)colorGreen); //does the turn left algorithm until it detects black or green
-        }
-        else //accounts for turning error in both directions
-      	{
-      				bool misaligned = true;
-      				while(misaligned)
-      				{
-                rotateRobot(10, 25);
-                if (SensorValue[S3] == (int)colorBlack || SensorValue[S3] == (int)colorGreen)
-                    misaligned = false;
-            	}
-    		}
-  	}
-    /*
-    need to introduce a secondary objective: pick up block and sort it
-
-    could introduce a way to recognize turns and adjust accordingly
-    could introduce A* algorithm to solve maze
-    */
-    displayString(5, "Maze Solved!");
-    displayString(6, "Time: %f s", timetofinish);
-    wait1Msec(10000);
 }
