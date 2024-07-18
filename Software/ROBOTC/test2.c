246383dad1ef8bfa70b6d44a4c2bd75b004b0559
zachb05 <zachary.barandino@gmail.com>
2024-07-18 18:26:45 -0400

    Finish Mapping, DFS

--- Software/ROBOTC/test2.c
+++ Software/ROBOTC/test2.c
@@ -1,5 +1,5 @@
-#define MAZE_SIZE 10
-#define MAX_STACK_SIZE 100
+#define MAZE_SIZE 6
+#define MAX_STACK_SIZE 70
 // Mapping vars
 short maze[MAZE_SIZE * MAZE_SIZE]; // ROW * MAZE_SIZE + COL to collape a 2d array into 1d
 bool isVisited[MAZE_SIZE * MAZE_SIZE];
@@ -7,7 +7,7 @@ short moves[MAX_STACK_SIZE];
 short path[MAX_STACK_SIZE];
 short currRow = 0, currCol = 0, direction = 2; // Start at top left (0,0); direction (N,0)(E,1)(S,2)(W,3)
 // Order: WSEN
-const short dRow[] = { 0, -1, 0, 1 };
+const short dRow[] = { 0, 1, 0, -1 };
 const short dCol[] = { 1, 0, -1, 0 };
 
 // Stack
@@ -15,44 +15,37 @@ typedef struct {
   short elements[MAX_STACK_SIZE]; // array of elements
   short size; // current size of the stack
 } mStack;
-
 bool push(mStack *stack, short value);
-
 short pop(mStack *stack);
-
 bool isEmpty(mStack *stack);
-
 bool isFull(mStack *stack);
-
 // DFS
 bool isValid(short row, short col);
-
 void dfs(short *maze, short row, short col);
-
 // Mapping
-
 short getRow(short data);
-
 short getCol(short data);
-
 short getDirection(short curr, short next);
-
 void addNode(short *maze);
 
 task main() {
+
 		for (short row = 0; row < MAZE_SIZE; row++) {
 				for (short col = 0; col < MAZE_SIZE; col++) {
 					maze[row * MAZE_SIZE + col] = (short)colorBlack;
 				}
 		}
-		maze[90] = (int)colorGreen;
-
+		maze[35] = (int)colorRed;
+		for (short i = 0; i < MAX_STACK_SIZE; i++) {
+				path[i] = -1;
+		}
 		dfs(maze, currRow, currCol);
 		for(short i = 0; i < MAX_STACK_SIZE; i++) {
-				writeDebugStreamLine("%f", path[i]);
+				writeDebugStreamLine("%hd", path[i]);
 		}
 
 		while(!getButtonPress(buttonEnter)) {}
+
 }
 
 bool push(mStack *stack, short value)
@@ -92,13 +85,13 @@ bool isFull(mStack *stack)
 }
 
 bool isValid(short row, short col) {
-		short color = maze[row * MAZE_SIZE + col];
     // If cell is out of bounds
     if (row < 0 || col < 0 || row >= MAZE_SIZE || col >= MAZE_SIZE)
         return false;
     // If the cell is already visited
     if (isVisited[row * MAZE_SIZE + col])
         return false;
+    short color = maze[row * MAZE_SIZE + col];
     // If color not black, green, or red, not valid
     if (color != (int)colorBlack && color != (int)colorRed && color != (int)colorGreen)
 				return false;
@@ -109,8 +102,10 @@ void dfs(short *maze, short row, short col) {
 	  // Initialize path variables
 		for (short i = 0; i < MAX_STACK_SIZE; i++) {
 				moves[i] = 0;
+				path[i] = -1;
+		}
+		for (short i = 0; i < MAZE_SIZE * MAZE_SIZE; i++) {
 				isVisited[i] = false;
-				path[i] = '0';
 		}
 		short moveCounter = 0, pathCounter = 0;
 		short finish = 0;
@@ -124,7 +119,6 @@ void dfs(short *maze, short row, short col) {
 		while(!isEmpty(stack)) {
 				// Pop top
 				short curr = pop(stack);
-
 				// Check finish
 				if (maze[curr] == (int)colorRed) {
 						finish = curr;
@@ -138,22 +132,27 @@ void dfs(short *maze, short row, short col) {
 
 				// Push all adj cells
 				for (short i = 0; i < 4; i++) {
-            short adjx = row + dRow[i];
-            short adjy = col + dCol[i];
+            short adjx = curr / MAZE_SIZE + dRow[i];
+            short adjy = curr % MAZE_SIZE + dCol[i];
 
-            short next = maze[adjx * MAZE_SIZE + adjy];
+            short next = adjx * MAZE_SIZE + adjy;
             // Check if valid
 						if (!isValid(adjx, adjy))
 								continue;
 
 						moves[moveCounter] = next * 100 + curr; // Tracks moves by child_curr
-            if(!push(stack, adjx * adjy + adjy))
+						moveCounter++;
+            if(!push(stack, adjx * MAZE_SIZE + adjy))
             		stopAllTasks();
         }
 		}
 		// Find path to finish
-		short reversePath[MAX_STACK_SIZE];
 		short cell = finish;
+		short reversePath[MAX_STACK_SIZE];
+		for (short i = 0; i < MAX_STACK_SIZE; i++) {
+				reversePath[i] = -1;
+		}
+
 		while (cell != row * MAZE_SIZE + col) {
 				for (short i = 0; i < MAX_STACK_SIZE; i++) {
 						if (moves[i] / 100 == cell) { // Find cell
@@ -181,7 +180,6 @@ short getCol(short data) {
 short getDirection(short curr, short next) {
 	short rowDiff = getRow(next) - getRow(curr);
 	short colDiff = getCol(next) - getCol(curr);
-
 	if (rowDiff == -1 && colDiff == 0) {
 			return 0;
 	} else if (rowDiff == 0 && colDiff == 1) {
@@ -191,7 +189,7 @@ short getDirection(short curr, short next) {
 	} else if (rowDiff == 0 && colDiff == -1) {
 			return 3;
 	} else {
-			return '-';
+			return -1;
 	}
 }
 
